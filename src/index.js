a86d1ed326541df68b087807b3772dc6e6f02bcf
quyDev <94169574+quy-benho@users.noreply.github.com>
2023-07-14 09:30:01 +0700

    add pretter husky lint-staged

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const morgan =  require('morgan');
-const path = require('path') 
-const { engine } = require('express-handlebars');
-const app = express();
+const express = 
+require('express');
+const morgan = 
+require('morgan');
+const path = 
+require('path');
+const { engine } =
+ require('express-handlebars');
+const app = 
+express();
 const port = 3000;
-const route = require('./routes')
-
-
+const route = 
+require('./routes');
 
 // cấu hình file tĩnh
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-// sử dụng middelware lấy body trong 
-app.use(express.urlencoded({
-    extended: true
-}));
+// sử dụng middelware lấy body trong
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 // XMLhttpreq fetch axios
@@ -22,10 +28,10 @@ app.use(express.json());
 // http logger
 // app.use(morgan('combined'))
 //template eg
-app.engine('hbs', engine({ extname: '.hbs', defaultLayout: "main"}));
+app.engine('hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
-app.set("views", path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
 route(app);
-app.listen(port, () =>  console.log(`Example app listening on port ${port}`))
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
